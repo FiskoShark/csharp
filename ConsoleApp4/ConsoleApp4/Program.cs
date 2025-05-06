72d64a56681e9d9baa0e9cd182dbe9a91f638fde
FiskoShark <vasyajon31@gmail.com>
2025-05-06 12:12:05 +0300

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,33 @@
+﻿class Number
+{
+    static void Main()
+    {
+        Console.Write("введіть число A: ");
+        int A = int.Parse(Console.ReadLine());
+
+        Console.Write("введіть число B: ");
+        int B = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("\nпарні числа, від A до B: ");
+        for (int i = A; i <= B; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        Console.WriteLine("\n\nнепарні числа від A до B: ");
+        int j = A;
+        while (j <= B)
+        {
+            if (j % 2 != 0)
+            {
+                Console.Write(j + " ");
+            }
+            j++;
+        }
+
+        Console.WriteLine();
+    }
+}
