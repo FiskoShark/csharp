bf74fd7d64ca53a9cc9b720aea2589063fd54272
FiskoShark <vasyajon31@gmail.com>
2026-07-18 22:57:14 +0200

    Add files via upload

---
+++ Lab_Threads/Task1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the start: ");
+            int start = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the end: ");
+            int end = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the number of threads: ");
+            int threadCount = int.Parse(Console.ReadLine());
+
+
+            Thread[] threads = new Thread[threadCount];
+
+            int range = end - start + 1;
+            int part = range / threadCount;
+
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                int threadStart = start + i * part;
+                int threadEnd;
+
+                if (i == threadCount - 1) {
+                    threadEnd = end;
+                }
+
+                else {
+                    threadEnd = threadStart + part - 1;
+                }
+
+                threads[i] = new Thread(() => {
+                    DoWork1(threadStart, threadEnd);
+                });
+
+                threads[i].Start();
+            }
+
+
+            for (int i = 0; i < threadCount; i++) { 
+                threads[i].Join();
+            }
+
+
+            Console.WriteLine("All threads have completed their work.");
+        }
+
+
+        static void DoWork1(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
---
+++ Lab_Threads/Task2/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = new int[10000];
+            Random random = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(1, 1000);
+            }
+
+
+            int max = 0;
+            int min = 0;
+            double average = 0;
+
+
+            Thread maxThread = new Thread(() =>
+            {
+                FindMax(numbers, ref max);
+            });
+
+            Thread minThread = new Thread(() =>
+            {
+                FindMin(numbers, ref min);
+            });
+
+            Thread averageThread = new Thread(() =>
+            {
+                FindAverage(numbers, ref average);
+            });
+
+
+
+            maxThread.Start();
+            minThread.Start();
+            averageThread.Start();
+
+            maxThread.Join();
+            minThread.Join();
+            averageThread.Join();
+
+
+
+            Thread fileThread = new Thread(() =>
+            {
+                WriteToFile(numbers, max, min, average);
+            });
+
+
+            fileThread.Start();
+            fileThread.Join();
+
+            Console.WriteLine("Готово!");
+
+        }
+
+        static void FindMax(int[] numbers, ref int max)
+        {
+            max = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+            Console.WriteLine($"Maximum: {max}");
+        }
+
+        static void FindMin(int[] numbers, ref int min)
+        {
+            min = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+            }
+            Console.WriteLine($"Minimum: {min}");
+        }
+
+        static void FindAverage(int[] numbers, ref double average)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            average = sum / numbers.Length;
+            Console.WriteLine($"Average: {average}");
+        }
+
+        static void WriteToFile(int[] numbers, int max, int min, double average)
+        {
+            using (StreamWriter writer = new StreamWriter("results.txt"))
+            {
+                writer.WriteLine("МаNumbers array:");
+
+                foreach (int number in numbers)
+                {
+                    writer.Write(number + " ");
+                }
+
+                writer.WriteLine();
+
+                writer.WriteLine($"Maximum: {max}");
+                writer.WriteLine($"Minimum: {min}");
+                writer.WriteLine($"Average: {average}");
+
+                Console.WriteLine("Results saved to file.");
+            }
+        }
+    }
+}
+
+
